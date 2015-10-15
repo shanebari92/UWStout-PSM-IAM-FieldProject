b57c0dd9d596fc9f8a01fcaf468d6e75150fc3cb
Shane Bari <shanebari92@gmail.com>
2015-10-15 15:21:44 -0500

    Quantmod Intro
    Quantmod is an R package to import and manipulate financial data from
    multiple sources (Yahoo finance, Federal Reserve, etc.)

---
+++ Quantmod intro.R
@@ -0,0 +1,17 @@
+
+#download data from yahoo finance
+getSymbols.yahoo('GS',from='2015-01-01',to=Sys.Date())
+
+#check to see if data contains Open,High,Low,Close,Volume,Adjusted
+print('OHLC?')
+is.OHLC(GS)
+
+print('Volume?')
+has.Vo(GS)
+
+print('Adjusted?')
+has.Ad(GS)
+
+#where and what was the high point?
+seriesHi(GS)
+
